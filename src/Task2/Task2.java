006572288efeb67bc0d9e61c2aa1c760ce2c1065
Admin <funedy@yandex.ru>
2024-01-02 03:56:50 +0300

    micro fix

--- src/Task2/Task2.java
+++ src/Task2/Task2.java
@@ -1,13 +1,14 @@
 package Task2;
 
-import java.util.Arrays;
+
 import java.util.List;
 import java.util.Map;
+
 
 public class Task2 {
     public static List<String> print(Map<Integer,String> stringMap){
         List<String> list = stringMap.entrySet().stream()
-                .filter(x -> Arrays.asList(1, 2, 5, 8, 9, 13).contains(x.getKey()))
+                .filter(x -> List.of(1, 2, 5, 8, 9, 13).contains(x.getKey()))
                 .filter(i -> i.getValue().length() % 2 != 0)
                 .map(i -> new StringBuilder(i.getValue()).reverse().toString())
                 .toList();
